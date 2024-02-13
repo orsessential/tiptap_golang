6778d2b8ea1c44780ced47f098a4f254f44f24b5
olivia30 <olivia@gramedia.id>
2024-02-13 19:30:49 +0700

    task 2

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sesi-dua
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	buf := bufio.NewReader(os.Stdin)
+	fmt.Println("Input Your Words: ")
+	wrd, _ := buf.ReadString('\n')
+	wrd = strings.TrimSpace(wrd)
+	str_wrd := strings.Split(wrd, "")
+	var new_map = map[string]int{}
+	for _, val := range str_wrd {
+		fmt.Println(val)
+		value, exist := new_map[val]
+		if exist {
+			new_map[val] = value + 1
+		} else {
+			new_map[val] = 1
+		}
+	}
+	fmt.Println(new_map)
+}
