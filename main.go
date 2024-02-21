88948c33e2a6e5c2d0d49b20d092e28bf499700c
olivia30 <olivia@gramedia.id>
2024-02-21 13:26:42 +0700

    fix handler without argument

--- main.go
+++ main.go
@@ -15,11 +15,15 @@ type Biodata struct {
 }
 
 type NewData struct {
-	ID        string
+	ID        int
 	Nama      string
 	Alamat    string
 	Pekerjaan string
 	Alasan    string
+}
+
+func isEqual(key1, key2 string) bool {
+	return strings.EqualFold(key1, key2)
 }
 
 func main() {
@@ -28,23 +32,28 @@ func main() {
 		{Nama: "Dian", Alamat: "Medan", Pekerjaan: "QA", Alasan: "Alasan2"},
 		{Nama: "Putri", Alamat: "Malang", Pekerjaan: "Developer", Alasan: "Alasan3"},
 	}
-	inputKey := os.Args[1:]
-	input_key := inputKey[0]
 	var new_data = NewData{}
 	bool_var := true
-	for key, value := range biodata {
-		keyStr := strconv.Itoa(key)
-		if input_key == keyStr || strings.ToLower(input_key) == strings.ToLower(value.Nama) {
-			new_data.ID = keyStr
-			new_data.Nama = value.Nama
-			new_data.Alamat = value.Alamat
-			new_data.Alasan = value.Alasan
-			new_data.Pekerjaan = value.Pekerjaan
-			fmt.Printf("%+v\n", new_data)
-			bool_var = false
+	if len(os.Args[1:]) == 0 {
+		bool_var = false
+		fmt.Println("Tolong Masukkan Nama atau Nomor Absen.\nContoh: go run main.go Olivia atau go run main.go 3")
+	} else {
+		inputKey := os.Args[1:]
+		input_key := inputKey[0]
+		for key, value := range biodata {
+			num, err := strconv.Atoi(input_key)
+			if (num == key && err == nil) || isEqual(input_key, value.Nama) {
+				new_data.ID = key
+				new_data.Nama = value.Nama
+				new_data.Alamat = value.Alamat
+				new_data.Alasan = value.Alasan
+				new_data.Pekerjaan = value.Pekerjaan
+				bool_var = false
+				fmt.Printf("%+v\n", new_data)
+			}
 		}
 	}
 	if bool_var {
-		fmt.Printf("Data dengan nama/absen tsb tidak tersedia")
+		fmt.Printf("Data dengan nama/absen tsb tidak tersedia\n")
 	}
 }
