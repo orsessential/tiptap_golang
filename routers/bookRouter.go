156922cf2ec7b4fa00d3b73646c0484bd1101076
olivia30 <olivia@gramedia.id>
2024-03-12 09:37:09 +0700

    api file upload

---
+++ routers/bookRouter.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"gin-framework/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartServer() *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/book", controllers.CreateBook)
+	router.GET("/book/:bookID", controllers.GetBook)
+	router.PUT("/book/:bookID", controllers.UpdateBook)
+	router.DELETE("/book/:bookID", controllers.DeleteBook)
+	router.GET("/book/allBooks", controllers.GetAllBooks)
+
+	return router
+}
